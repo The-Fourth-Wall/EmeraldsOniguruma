3f6c8579d5b11c080df004bbaadd87bb56cbefa3
oblivious <thanos.papap42@gmail.com>
2024-07-29 22:27:28 +0300

    Added legals.

--- src/EmeraldsOniguruma.h
+++ src/EmeraldsOniguruma.h
@@ -1,3 +1,21 @@
+/**
+ * Wrapper for the Oniguruma Regular Expression Library
+ * Copyright (C) 2024  oblivious
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef __EMERALDS_ONIGURUMA_H_
 #define __EMERALDS_ONIGURUMA_H_
 
