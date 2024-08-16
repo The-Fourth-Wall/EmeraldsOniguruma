affcb7899ec1edf956264a793d9b850c7c3259f9
oblivious <thanos.papap42@gmail.com>
2024-08-16 12:57:25 +0300

    Migrated to new cSpec.

--- spec/EmeraldsOniguruma.spec.c
+++ spec/EmeraldsOniguruma.spec.c
@@ -1,6 +1,6 @@
 #include "../libs/cSpec/export/cSpec.h"
 #include "oniguruma/oniguruma.spec.h"
 
-spec_suite({ T_oniguruma(); });
-
-int main(void) { run_spec_suite("all"); }
+int main(void) {
+  cspec_run_suite("all", { T_oniguruma(); });
+}
